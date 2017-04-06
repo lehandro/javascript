a92044931167a2ac53f5eb9f7fb16e315e2747f1
lehandro <lehandro@protonmail.ch>
2017-04-05 20:07:24 -0400

    'javascript'

---
+++ main.js
@@ -0,0 +1,59 @@
+// /*
+// .*tIPOS DE DATOS js
+// .*
+// .*pRINCIPALMENTE SON 3:
+// .*BOOLEANS
+// .*... LOS BOOLEANS PUEDEN TENER SOLO 2 VALORES: TRUE OR FALSE
+// .*NUMBERS
+// .*...NUMEROS QUE PERTENECEN A LOS NUMEROS REALES
+// .*STRINGS
+// .*...SON CADENAS DE CARACTERES - NOMBRE COMPLETO DE ALGUIEN, INCLUYENDO ESPACIOS
+//  */
+
+// //DEFINIENDO BOOLEANS
+// var MivariableBoolean = true
+// var Mivariableboolean2 = false
+// console.log("imprimiendo booleans", MivariableBoolean, Mivariableboolean2)
+
+// // DEFINIENDO NUMEROS
+// var Minumero = -17
+// var Minumero2 = 56
+// var Minumero3 = 129129129192
+// var Minumero4 = 98989898
+// var Minumero5 = 0.98
+// var Minumero6 = 7.9934
+// console.log("imprimiendo numeros", Minumero, Minumero2, Minumero3, Minumero4)
+
+// //DEFINIENDO STRINGS
+// var Mistrings ="mi strings"
+// var Mistrings2 = "puede conter numeros como 123456789&%"
+// var Mistrings3 = "2"
+// console.log("imprimiendo strings",Mistrings, Mistrings2, Mistrings3 )
+
+// var miNuevoNumero = Number(Minumero)
+// console.log(typeof miNuevoNumero)
+
+// console.log("suma", 1 + 3)
+// console.log("restar", 1 - 3)
+// console.log("multiplicar", 9 * 3)
+// console.log("dividir", 28 / 7)
+
+// var suma = 5 + 5
+// var MensajeParaElUsuario = "El resultado de la suma es:" + suma
+// alert(MensajeParaElUsuario)
+
+// var edad = prompt("Ingresa tu edad")
+// var nombre = prompt("Ingresa tu Nombre")
+
+// alert("Hola!" + nombre + ", tienes" + edad + "años")
+
+//crear un programa que consulte el nombre el apellido, la edad y que tambien consulte cuantos años mas quisiera tener
+
+var edad = prompt("Cual es tu edad?")
+var nombre = prompt("Cual es tu nombre?")
+var apellido = prompt("Cual es tu apellido?")
+var masedad = prompt("Cuantos años mas deseas tener?")
+var sumar = Number(edad)+Number(masedad)
+console.log("edad", "masedad")
+alert("tu deseas tener" + sumar + "fin")
+
